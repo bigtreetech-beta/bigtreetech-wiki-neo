8177def22314b3b727c2ee2f92263eb7d4e99ec9
Neko-vecter <vecterfang@icloud.com>
2026-01-26 09:17:33 -0500

    wiki: update homepage

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -2,70 +2,53 @@ import type {ReactNode} from 'react';
 import clsx from 'clsx';
 import Heading from '@theme/Heading';
 import styles from './styles.module.css';
+import Translate from '@docusaurus/Translate';
 
 type FeatureItem = {
-  title: string;
-  // Svg: React.ComponentType<React.ComponentProps<'svg'>>;
+  title: ReactNode;
+  prod_img: string;
+  url: string;
   description: ReactNode;
 };
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Board',
-    // Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    title: <Translate>Board</Translate>,
+    prod_img: require('@site/static/img/maxez-main.webp').default,
+    url: 'docs/intro',
     description: (
       <>
-        <a href="./docs/intro">
-          <img
-            src={require('@site/static/img/maxez-main.webp').default}
-            alt=""
-          />
-          <br/>
-        </a>
       </>
     ),
   },
   {
-    title: 'Extruder',
-    // Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    title: <Translate>Extruder</Translate>,
+    prod_img: require('@site/static/img/h2-v2x.webp').default,
+    url: 'docs/extruder',
     description: (
       <>
-        <a href="./docs/extruder">
-          <img
-            src={require('@site/static/img/h2-v2x.webp').default}
-            alt=""
-          />
-          <br/>
-        </a>
       </>
     ),
   },
   {
-    title: 'Module',
-    // Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    title: <Translate>Module</Translate>,
+    prod_img: require('@site/static/img/lis2dw-main.webp').default,
+    url: 'docs/module',
     description: (
       <>
-        <a href="./docs/module">
-          <img
-            src={require('@site/static/img/lis2dw-main.webp').default}
-            alt=""
-          />
-          <br/>
-        </a>
       </>
     ),
   },
 ];
 
-// function Feature({title, Svg, description}: FeatureItem) {
-
-function Feature({title, description}: FeatureItem) {
+function Feature({title, prod_img, url, description}: FeatureItem) {
   return (
     <div className={clsx('col col--4')}>
-      {/* <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
-      </div> */}
-
+      <div className="text--center">
+        <a href={url}>
+          <img src={prod_img} width="65%" />
+        </a>
+      </div>
       <div className="text--center padding-horiz--md">
         <Heading as="h3">{title}</Heading>
         <p>{description}</p>
